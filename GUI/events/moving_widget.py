d612d93394e03a8549de50e9a7f5d4f7c97d0f9e
texaskyle <evansnjoroge3345@gmail.com>
2023-01-06 23:03:45 +0300

    code for moving a widget on a screen

--- GUI/events/moving_widget.py
+++ GUI/events/moving_widget.py
@@ -2,32 +2,37 @@ from tkinter import *
 
 
 def move_up(event):
-    label1.place(x=label1.winfo_x(), y=label1.winfo_y()-10)
+    label.place(x=label.winfo_x(), y=label.winfo_y()-10)
 
 
 def move_down(event):
-    pass
+    label.place(x=label.winfo_x(), y=label.winfo_y()+10)
 
 
 def move_left(event):
-    pass
+    label.place(x=label.winfo_x()-10, y=label.winfo_y())
 
 
 def move_right(event):
-    pass
+    label.place(x=label.winfo_x()+10, y=label.winfo_y())
 
 
 window = Tk()
 
-car_image = PhotoImage(file='C:\\Users\\charles\\Pictures\\Audi-Car-Real-PNG.png')
+race_car = PhotoImage(file="C:\\Users\\charles\\Pictures\\Audi-Car-Real-PNG.png")
+label = Label(window, image=race_car)
+label.place(x=0, y=0)
 
-label1 = Label(window, image=car_image)
-label1.place(x=0, y=0)
+window.bind("<w>", move_up)
+window.bind("<s>", move_down)
+window.bind("<a>", move_left)
+window.bind("<d>", move_right)
+window.bind("<Up>", move_up)
+window.bind("<Down>", move_down)
+window.bind("<Left>", move_left)
+window.bind("<Right>", move_right)
 
-label1.bind("<w>", move_up)
-label1.bind("<s>", move_down)
-label1.bind("<a>", move_left)
-label1.bind("<d>", move_right)
+
 
 window.geometry('500x500')
 
